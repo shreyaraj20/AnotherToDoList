258f300c39f4a0f4653ccd61d7ac537330588a03
Shreya Rajput <15ucs134@lnmiit.ac.in>
2020-04-27 00:49:07 -0700

    List Created

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./ListItems.css";
+import { FaTrash } from "react-icons/fa";
 
 function ListItems(props) {
   const items = props.items;
   const listItems = items.map(item => {
     return (
       <div className="list" key="item.key">
-        <p>{item.text}</p>
+        <p>
+          {item.text}
+          <span>
+            <FaTrash />
+          </span>
+        </p>
       </div>
     );
   });
